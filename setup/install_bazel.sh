8ad37d668e12ec380babd40b53dbc5b6563a03b7
linji.xue <linji.xue@graviti.cn>
2019-10-29 17:51:26 +0800

    [setup] Refine install scripts for 'bazel' and 'cppcheck'

--- setup/install_bazel.sh
+++ setup/install_bazel.sh
@@ -9,8 +9,10 @@ set -v
 # Install Bazel #
 #################
 
-sudo apt-get update
-sudo apt-get install -y openjdk-8-jdk curl
+if ! dpkg -s openjdk-8-jdk curl 1>/dev/null 2>&1; then
+    sudo apt-get update
+    sudo apt-get install -y openjdk-8-jdk curl
+fi
 
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" |
     sudo tee /etc/apt/sources.list.d/bazel.list
