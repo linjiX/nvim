f46a1d8064be82b302d18ddfd32b4f9306da3f07
linji.xue <linji.xue@graviti.cn>
2020-01-17 14:31:32 +0800

    [setup] Use 'FILE' to store download file name for 'install_hadolint.sh'

--- setup/install_hadolint.sh
+++ setup/install_hadolint.sh
@@ -9,7 +9,8 @@ set -v
 # Install hadolint #
 ####################
 
-VERSION=v1.17.4
+VERSION="v1.17.4"
+FILE="hadolint-Linux-x86_64"
 
 if ! dpkg -s wget 1>/dev/null 2>&1; then
     sudo apt-get update
@@ -18,8 +19,8 @@ fi
 
 TMPDIR="$(mktemp -d /tmp/install_hadolint.XXXX)"
 pushd "$TMPDIR" >/dev/null
-wget https://github.com/hadolint/hadolint/releases/download/$VERSION/hadolint-Linux-x86_64
-chmod +x hadolint-Linux-x86_64
-sudo mv hadolint-Linux-x86_64 /usr/local/bin/hadolint
+wget https://github.com/hadolint/hadolint/releases/download/$VERSION/$FILE
+chmod +x $FILE
+sudo mv $FILE /usr/local/bin/hadolint
 
 popd >/dev/null
