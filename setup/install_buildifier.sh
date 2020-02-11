047921280bf59d2808e602e2639616c5a0222d24
linji.xue <linji.xue@graviti.cn>
2020-02-11 22:05:49 +0800

    [setup] Add apt 'software-properties-common' & 'git' check in 'golang'

--- setup/install_buildifier.sh
+++ setup/install_buildifier.sh
@@ -6,6 +6,12 @@ set -e
 set -v
 
 if ! command -v go 1>/dev/null 2>&1; then
+    if ! dpkg -s git software-properties-common 1>/dev/null 2>&1; then
+        sudo apt-get update
+        sudo apt-get install -y \
+            software-properties-common \
+            git
+    fi
     sudo apt-add-repository -y ppa:longsleep/golang-backports
     sudo apt-get update
     sudo apt-get install -y golang-go
